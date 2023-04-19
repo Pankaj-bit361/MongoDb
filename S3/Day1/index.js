4aa7e3ec6a3c7c68088e50c5a34e44004bc8b405
Pankaj-bit361 <pnkjvshst1@gmail.com>
2023-04-19 18:12:24 +0530

    done

---
+++ S3/Day1/index.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose")
+
+
+const main=async()=>{
+    try{
+        const connection=mongoose.connect("mongodb://127.0.0.1:27017/learnmongo")
+        console.log("connected to the DB")
+        const user=new Usermodel({
+            name:"rohit",
+            age:"35",
+            city:"delhi"
+
+        })
+
+        await user.save()
+    }catch(err){
+console.log("Cannot connect to DB")
+console.log(err)
+    }
+    
+}
+const userSchema=mongoose.Schema({
+    name:{type:String,require:true},
+    age:{type:Number,require:true},
+    city:{type:String,require:true}
+},{
+    versionKey:false
+})
+
+const Usermodel=mongoose.model("user",userSchema)
+
+
+main()
